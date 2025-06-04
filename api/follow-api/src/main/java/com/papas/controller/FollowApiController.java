99502e95feea6050b096bee0902cc4df36f4db6c
seongjin-an <seongjin4us@gmail.com>
2025-06-04 20:36:05 +0900

    feature: follow 도메인 작업 - follow 모델 및 엔티티 정의 - follow 디비 접근 기술 구현   - follow, unfollowBy, getFollowsByFromUserId, ... - follow 서비스 정의 및 구현 - follow api 앱 정의 및 구현

--- api/follow-api/src/main/java/com/papas/controller/FollowApiController.java
+++ api/follow-api/src/main/java/com/papas/controller/FollowApiController.java
@@ -1,18 +1,47 @@
 package com.papas.controller;
 
+import com.papas.Response;
+import com.papas.follow.Follow;
 import com.papas.follow.FollowUsecase;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @Slf4j
 @RequiredArgsConstructor
-@RequestMapping
+@RequestMapping("/follow")
 @RestController
 public class FollowApiController {
 
     private final FollowUsecase followUsecase;
-    
+
+    @PostMapping("/{fromUser}/{toUser}/follow")
+    public Response<Void> follow(@PathVariable("fromUser") Long fromUser, @PathVariable("toUser") Long toUser) {
+        followUsecase.follow(fromUser, toUser);
+        return Response.justOk();
+    }
+
+    @PostMapping("/{fromUser}/{toUser}/unfollow")
+    public Response<Void> unfollow(@PathVariable("fromUser") Long fromUser, @PathVariable("toUser") Long toUser) {
+        followUsecase.unfollowBy(fromUser, toUser);
+        return Response.justOk();
+    }
+
+    @PostMapping("/{followId}/unfollow")
+    public Response<Void> unfollow(@PathVariable("followId") Long followId) {
+        followUsecase.unfollowBy(followId);
+        return Response.justOk();
+    }
+
+    @GetMapping("/{fromUser}/following-list")
+    public Response<List<Follow>> followingList(@PathVariable("fromUser") Long fromUser) {
+        return Response.success(followUsecase.getFollowsByFromUserId(fromUser));
+    }
+
+    @GetMapping("/{toUser}/follower-list")
+    public Response<List<Follow>> followerList(@PathVariable("toUser") Long toUser) {
+        return Response.success(followUsecase.getFollowsByToUserId(toUser));
+    }
 }
